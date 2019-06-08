41d89335a0e1b3e6fce890eada78c92033e0dd06
Tri Vo <acumen.us@gmail.com>
2019-06-08 10:15:13 +0700

    Init project

---
+++ flutter_calayer/lib/calayer_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class CALayerPlayer extends StatefulWidget {
+  @override
+  _CALayerPlayerState createState() => _CALayerPlayerState();
+}
+
+class _CALayerPlayerState extends State<CALayerPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
